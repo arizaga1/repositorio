24ddafa1bf8d920f2745876391fdd72b55a8ad58
arizaga1 <arizaga@gmail.com>
2022-03-23 17:05:32 -0600

    cambios menores

--- serial_i2c/main.h
+++ serial_i2c/main.h
@@ -1,9 +1,9 @@
 #include <18F4550.h>
 #device ADC=10
 
-#FUSES NOWDT                 	//No Watch Dog Timer
+#FUSES NOWDT                    //No Watch Dog Timer
 
 #use delay(crystal=4000000)
 #use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8,stream=PORT1)
-#use i2c(Master,Fast,sda=PIN_B0,scl=PIN_B1,force_hw)
+#use i2c(Master,Fast,sda=PIN_B0,scl=PIN_B1,force_hw)   //módulo serial del microcontrolador
 
