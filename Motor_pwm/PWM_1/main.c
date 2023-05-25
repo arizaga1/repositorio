ac4f63deddb55b71bc59164ffbb326ec4273aa01
arizaga1 <arizaga@gmail.com>
2023-05-25 12:55:54 -0500

    Agregar comentarios al código
    nada, solo comentarios menores

--- Motor_pwm/PWM_1/main.c
+++ Motor_pwm/PWM_1/main.c
@@ -1,14 +1,15 @@
-#include <main.h>
-#INCLUDE <math.h>
+#include <main.h> //se agrregan funciones o variables a programas
+#include <math.h> //se agregan funciones matemáticas 
 //PWM  Modulación por ancho de Pulso
 //se utiliza para control de velocidad 
 //de motores de CD
-float contador=0;
+float contador=0; 
 void main()
 {
    set_tris_a(0x02); 
-   //para colocar los pines del puerto C como entrad o salida
-   //0x02 pone en 1 lógico los pines c0 y C1 por lo tanto seran entradas
+   //para colocar los pines del puerto A como entrada (1) o salida (0)
+   //0x02 =b'00000010  pone en 1 lógico el pin A1 
+   //por lo tanto será de entrada
     setup_timer_2(T2_DIV_BY_16,249,1);      //1.0 ms overflow, 1.0 ms interrupt
 
    setup_ccp1(CCP_PWM);
@@ -21,11 +22,12 @@ void main()
   while(TRUE)
    {
 
- If(input(PIN_A1))
+ If(input(PIN_A1)) //botón de entrada para cambiar el ancho
+ //de pulso del PWM 
  {
       contador=contador+1; 
       set_pwm1_duty((int16)contador*100);
-      delay_ms(200);
+      delay_ms(200); //delay agregado para evitar rebotes
       //TODO: User Code
    }
    
